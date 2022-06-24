a5ee96e04114dbaacfea7d05df0527f2d4a26917
blackcatcoder <86876350+blackcatcoder@users.noreply.github.com>
2022-06-24 22:06:42 +0700

    commit section 2 of max
    section 2 description

---
+++ my-app/src/app/servers/servers.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  //selector: '[app-servers]',
+  //selector: '.app-servers',
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  allowNewServer = false;
+  serverCreated = false;
+  serverName="default serverName";
+  serverName2="default serverName2"
+  serverName3 = "";
+  servers = ["server 1", "server 2"];
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 10000);
+   }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreated = !this.serverCreated;
+    this.servers.push(this.serverName3);
+  }
+
+  onUpdateServerName(event: Event){
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+}
