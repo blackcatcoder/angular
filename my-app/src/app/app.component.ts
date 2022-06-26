850169a4fe67b7a8bf8e71302094e2eb12214934
blackcatcoder <86876350+blackcatcoder@users.noreply.github.com>
2022-06-26 10:36:08 +0700

    section 5 component databinding deep dive

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -8,4 +8,31 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-app';
   name='anhvi';
+
+  serverElements: any[] = [{type: 'server', name: 'server', content: 'content1'}];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}){
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    })
+  }
+
+  onBlueprintAdded(serverData: {serverName: string, serverContent: string}){
+    this.serverElements.push({
+      type: 'blueprint',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    })
+  }
+
+  onChangeFirst(){
+    this.serverElements[0].name = 'changed'+Math.random();
+  }
+
+  onDestroyFirst(){
+    this.serverElements.splice(0, 1);
+  }
+  
 }
