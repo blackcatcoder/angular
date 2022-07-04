33bf0def6940facf8ea890325d15545907cfcdb9
blackcatcoder <vinv.2491@hotmail.com>
2022-07-04 16:26:55 +0700

    add company handson

---
+++ company-handson/src/app/employee-element/employee-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-element',
+  templateUrl: './employee-element.component.html',
+  styleUrls: ['./employee-element.component.css']
+})
+export class EmployeeElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
